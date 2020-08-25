59ff22aa271c239780cef7b3c01d2fd7ef42f8b6
NoNamePN88 <PetrovskijPN88@gmail.com>
2020-08-25 18:08:48 +0700

    app-style.css

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-//function Declaration
-function askUser (name){
-    console.log('Hi ', name);
+const person = {
+    lastName: 'Фамилия',
+    firstName: 'Имя',
+    secondName: 'Отчество',
+    telephonNumber: 'Номер телефона',
+    greet: function (){
+        console.log('Информация о пользователе.');
+    }
+}
+const hedding = document.getElementById('sprav');
+//const hedding2 = document.getElementsByTagName('h2')[0];
+const hedding2 = document.querySelector('h2');
+console.log(hedding2);
+
+function addStylesTo(node){
+
+        console.dir(hedding.textContent);
+        node.textContent = 'Справочная система';
+        node.style.color = 'gray';
+        node.style.backgroundColor = 'white';
+        node.style.borderRadius = '5px';
+        node.style.padding = '1rem';
+
 }
 
-const userName='user';
-askUser (userName);
+setTimeout(() =>{
+    addStylesTo(hedding);
+}, 1500)
 
-//function Expression
-const greet = function greet(name){
-    console.log('Hay, you', name);
-}
-greet(userName)
-
-console.log(typeof  askUser);
-console.dir(askUser);
-console.log(typeof greet);
